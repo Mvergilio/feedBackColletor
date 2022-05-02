2045bb102be64d0542218a78a7afee861ee4cf06
Mvergilio <mvergiliodesouza@gmail.com>
2022-05-02 18:27:48 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="bg-green-400 p-2">hello Word</h1>
+    </div>
+  );
+}
+
+export default App;

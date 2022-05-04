f2d7606358692ac9635c15e35423727f60bd8334
Mvergilio <mvergiliodesouza@gmail.com>
2022-05-04 18:32:36 -0300

    add the array of assets and buttons

--- src/components/WidGet.tsx
+++ src/components/WidGet.tsx
@@ -12,7 +12,7 @@ export function WidGet() {
         <WidGetForm />
       </Popover.Panel>
 
-      <Popover.Button className="group  flex items-center h-12 px-3  bg-brand-500 rounded-full">
+      <Popover.Button className="group  flex items-center h-12 px-3  bg-brand-500 rounded-full mt-4">
         <ChatTeardropDots size={24} color="white" weight="regular" />
         <span className="text-white font-sans text-base overflow-hidden max-w-0 group-hover:max-w-sm transition-all duration-500 ease-linear">
           <span className="pr-2"></span>
--- src/components/WidGetForm.tsx
+++ src/components/WidGetForm.tsx
@@ -1,14 +1,41 @@
+import { Popover } from "@headlessui/react";
 import { X } from "phosphor-react";
 import React from "react";
 
+const feedBackTypes = {
+  BUG: {
+    folderUrl: "../assets/bug.svg",
+    altText: "image de uma largata roxa olhando para frente",
+  },
+  IDEA: {
+    folgerUrl: "../assets/idea.svg",
+    altText: "Imagen de uma lampada acesa representando uma idea",
+  },
+  THOUGHT: {
+    folderURL: "../assets/thought.svg",
+    altText: "Imagem de uma nuvem de pensamento",
+  },
+};
 export function WidGetForm() {
   return (
-    <div className="bg-zinc-800 rounded-2xl w-[calc(100vw-2rem)] md:w-auto">
-      <header className="flex items-around">
-        <span>Deixe seu FeedBack </span>
-        <X className="text" />
+    <div className="relative bg-zinc-800 rounded-2xl w-[calc(100vw-2rem)] md:w-auto py-2 flex flex-col items-center">
+      <header className="bg-green-500 flex items-around">
+        <span className="text-white ">Deixe seu FeedBack </span>
+        <Popover.Button
+          className="text-zinc-500 hover:text-zinc-100 absolute top-3 right-3"
+          type="button"
+          title="Botão que fecha o painel"
+        >
+          <X weight="bold" />
+        </Popover.Button>
       </header>
-      <footer className="text-sm text-zinc-400">
+      <main>
+        {Object.entries(feedBackTypes).map(([key, value]) => {
+          console.log(key, value);
+          return null;
+        })}
+      </main>
+      <footer className="text-sm text-zinc-400 mt-4">
         Feito com ♥ por{" "}
         <a className="underline underline-offset-2" href="#">
           Marco Antonio
